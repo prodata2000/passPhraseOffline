cf7036cda275437f53fc29b135ec42c284770c55
prodata2000 <prodata2000@sparkforge.pro>
2023-09-11 13:39:21 -0400

    Created Readme and added a sample wordlist for other to see.

--- passphraseoffline.py
+++ passphraseoffline.py
@@ -1,12 +1,9 @@
 import random
 
-
-# Functions:
 def load_words_from_file(wordlist):
     """Load words from a file and return them as a list."""
     with open(wordlist, 'r') as f:
         return [word.strip() for word in f.read().split(',')]
-
 
 def create_string_of_words(words, target_length=35):
     filtered_words = [word for word in words if not (any(char.isdigit() for char in word) or '.' in word)]
@@ -20,8 +17,12 @@ def create_string_of_words(words, target_length=35):
                 break
     return line.strip()
 
+def replace_spaces_with_symbols(text):
+    """Replace spaces in the text with a random special symbol."""
+    symbols = "!@#$%^&*()[]{}<>"
+    return ''.join(random.choice(symbols) if char == ' ' else char for char in text)
 
-def generate_text_block(seed_value=None):
+def generate_text_block(seed_value=None, replace_spaces=False):
     if seed_value is None:
         seed_value = random.randint(0, 99999999999)
 
@@ -34,6 +35,8 @@ def generate_text_block(seed_value=None):
         line = create_string_of_words(words)
         while not (34 <= len(line) <= 35):
             line = create_string_of_words(words)
+        if replace_spaces:
+            line = replace_spaces_with_symbols(line)
         lines.append(f"{i} {line}")
 
     seed_line = f"{seed_value}"
@@ -41,10 +44,12 @@ def generate_text_block(seed_value=None):
 
     return '\n'.join(lines), seed_value
 
-
 def prompt_for_seed_and_generate_file():
     # Prompt the user for a seed value
     seed_value = input("Please enter a seed number (or press Enter to generate a random seed): ")
+
+    # Prompt the user to replace spaces with special characters
+    replace_spaces = input("Do you want to replace spaces with special characters? (yes/no): ").strip().lower() == 'yes'
 
     try:
         seed_value = int(seed_value)
@@ -52,13 +57,12 @@ def prompt_for_seed_and_generate_file():
         seed_value = None  # This will trigger the function to generate a random seed
 
     # Generate and save the text block
-    text_block, seed = generate_text_block(seed_value)
+    text_block, seed = generate_text_block(seed_value, replace_spaces)
     wordlist = f"{seed}.txt"
     with open(wordlist, 'w') as f:
         f.write(text_block)
 
     return wordlist
-
 
 if __name__ == "__main__":
     filename = prompt_for_seed_and_generate_file()
